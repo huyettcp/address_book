03a4a871a86c5756e26205fc28799e411b385531
huyettcp <huyettcp@gmail.com>
2013-06-28 10:51:35 -0400

    Revisions from demo

--- address_book.rb
+++ address_book.rb
@@ -1,65 +1,48 @@
-# use this code to create an address book in sinatra
-# create an input form
-# add a person
-# list people
+
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pg'
 require 'pry'
+require 'rainbow'
 
-puts "Hey baby, what do I call an angel like you?"
-first_name = gets.chomp
-puts "Can I at least get a last name?"
-last_name = gets.chomp
-puts "Well, how old are you?"
-age = gets.chomp
-puts "I'm adding you to my contacts"
-#put 3 variables, just for testing to make sure it works
-#cannot test because I cannot connect to database
-
-# this establishes a connection to the database
-# db = PG.connect(:dbname => 'address_book',
-#   :host => 'localhost')
-# executing sql code
-# passing a string of sql to the database
-
-# insert into database
-db = PG.connect(:dbname => 'address_book', :host => 'localhost')
-
-sql = "insert into contacts (first, last, age) values ('#{first}, #{second}, #{age})"
-db.exec(sql)
-db.close
 
 
 get '/' do
-
-  sql = "select (first, last, age) from contacts"
-  #not sure if parathases are needed
-  db.exec(sql) do |result|
-    result.each do |row|
-      @row << row
-    end
-  end
-  erb :contact
+  erb :index
 db.close
+end
+
+
+get '/contacts' do
+  db = PG.connect(:dbname => 'address_book', :host => 'localhost')
+  sql = "SELECT * FROM contacts"
+  @contacts = db.exec(sql)
+  db.close
+  erb :contacts
+end
 
 post '/contacts' do
   first = params[:first]
   last = params[:last]
   age = params[:age]
-  gender = params[:gender]
-  dtgd = params[:dtgd]
-  phone = params[:phone]
+  sql = 'INSERT INTO CONTACTS (first, last, age) VALUES ('#{first}', '#{last}', #{age})'
+  db = PG.connect(:dbname => 'address_book', :host => 'localhost')
+  db.exec(sql)
+  db.close
+  redirect to '/contacts'
+
 end
 
-# reads from database
-# db = PG.connect(:dbname => 'address_book',
-#   :host => 'localhost')
-# sql = "select first, age from contacts"
-# db.exec(sql) do |result|
-#   result.each do |row|
-#     puts row
-#   end
-# end
-# db.close
+get '/contacts/:name' do
+  @user_name = params[:name]
+  db = PG.connect(:dbname => 'address_book', :host => 'localhost')
+  sql = "SELECT * FROM contacts WHERE first = '#{@user_name}'"
+  @contact = db.exec(sql).first
+  db.close
+  erb :contact
+end
+
+get '/contacts/new' do
+  erb :form
+end
